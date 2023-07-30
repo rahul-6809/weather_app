10ec537cd1c87d5136e1e6f5162a046dce2a24f0
https://github.com/rahul-6809 <rkpandeyisin2015@gmail.com>
2023-07-30 14:35:27 +0530

    done

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f14c1f01592c6af2c165c63d447cc44d",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
